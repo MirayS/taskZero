5e28aa1b9fd56eba5dbbd3954a4892b509766ddf
MirayS <my@mirays.tech>
2021-01-08 00:30:05 +0300

    Added console command, added ProductData Service

---
+++ src/Command/ParseCsvCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\ProductData;
+use App\Helper\ExchangeRateHelper;
+use App\Parser\CsvParser;
+use App\Parser\FileNotFoundException;
+use App\Repository\ProductDataRepository;
+use App\Service\ProductDataService;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ParseCsvCommand extends Command
+{
+    protected static $defaultName = 'app:parse-csv';
+    /**
+     * @var ProductDataService
+     */
+    private ProductDataService $productDataService;
+
+    public function __construct(string $name = null, ProductDataService $productDataService)
+    {
+        parent::__construct($name);
+
+        $this->productDataService = $productDataService;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Parse CSV file to database')
+            ->addArgument('file', InputArgument::REQUIRED, 'path to the file to parse')
+            ->addArgument('test', InputArgument::OPTIONAL, 'Start in test mode')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $file = $input->getArgument('file');
+        $test = $input->getArgument('test');
+
+        if (!file_exists($file)) {
+            $io->error('File not exists');
+            return Command::FAILURE;
+        }
+
+        $result = $this->productDataService->parseDataFromFile($file, new CsvParser());
+        if (!isset($test))
+            $this->productDataService->saveRangeToDataBase($result);
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Service/ProductDataService.php
@@ -0,0 +1,108 @@
+<?php
+
+
+namespace App\Service;
+
+
+use App\Entity\ProductData;
+use App\Helper\DataHelper;
+use App\Helper\ExchangeRateHelper;
+use App\Parser\ParserInterface;
+use App\Repository\ProductDataRepository;
+use App\Validator\PriceAndCountValidator;
+use App\Validator\PriceValidator;
+use App\Validator\ValidatorInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ProductDataService
+{
+    /**
+     * @var DataHelper
+     */
+    private DataHelper $dataHelper;
+    /**
+     * @var EntityManagerInterface
+     */
+    private EntityManagerInterface $em;
+    /**
+     * @var ProductDataRepository
+     */
+    private ProductDataRepository $repository;
+
+    public function __construct(ProductDataRepository $repository, EntityManagerInterface $em, DataHelper $dataHelper) {
+
+        $this->dataHelper = $dataHelper;
+        $this->em = $em;
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param string $fileName
+     * @param ParserInterface $parser
+     * @return ProductData[]
+     */
+    public function parseDataFromFile(string $fileName, ParserInterface $parser): array {
+        $result = [];
+
+        foreach ($parser->parse($fileName) as $parsedRow) {
+            try {
+                if (count($parsedRow) < 6)
+                    continue;
+                $productCode = $this->dataHelper->parseString($parsedRow[0]);
+                if (count(array_filter($result, function($row) use($productCode) { return $row->getCode() == $productCode; })) != 0)
+                    continue;
+                $row = $this->repository->findOneByCode($productCode);
+                if ($row == null) {
+                    $row = new ProductData();
+                    $row->setAdded(new \DateTime());
+                    $row->setTimestamp(new \DateTime());
+                }
+                $row = $this->fillEntity($row, $parsedRow);
+                if ($this->validateEntity($row, [
+                    new PriceAndCountValidator(),
+                    new PriceValidator(),
+                ]))
+                $result[] = $row;
+            } catch (\Exception $exception) {
+
+            }
+        }
+        return $result;
+    }
+
+    private function fillEntity(ProductData $productData, array $parsedRow):ProductData {
+        $productData->setCode($parsedRow[0]);
+        $productData->setName($parsedRow[1]);
+        $productData->setDescription($parsedRow[2]);
+        $productData->setStock($this->dataHelper->parseCount($parsedRow[3]));
+        $productData->setPrice($this->dataHelper->parsePriceInDollars($parsedRow[4]));
+        if ($this->dataHelper->parseBool($parsedRow[5]))
+            $productData->setDiscontinued(new \DateTime());
+        return $productData;
+    }
+
+    /**
+     * @param ProductData $entity
+     * @param ValidatorInterface[] $validators
+     * @return bool
+     */
+    private function validateEntity(ProductData $entity, array $validators):bool {
+        foreach ($validators as $validator)
+        {
+            if (!$validator->validate($entity))
+                return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param ProductData[] $products
+     */
+    public function saveRangeToDataBase(array $products) {
+        foreach ($products as $product) {
+            $this->em->persist($product);
+        }
+        $this->em->flush();
+    }
+}
