f2d8d773ac432d3cda848770f2369dbc96c60967
MirayS <my@mirays.tech>
2021-01-08 00:28:24 +0300

    Added CSV parser

---
+++ src/Parser/CsvParser.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Parser;
+
+
+class CsvParser implements ParserInterface
+{
+    public function parse(string $filePath): \Generator
+    {
+        $row = 0;
+        $handle = fopen($filePath, 'r');
+        if ($handle === false)
+            throw new FileNotFoundException();
+
+        try {
+            while (($data = fgetcsv($handle)) !== false) {
+                if ($row !== 0) {
+                    yield $data;
+                }
+                $row++;
+            }
+        } finally {
+            fclose($handle);
+        }
+    }
+}
---
+++ src/Parser/FileNotFoundException.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Parser;
+
+
+class FileNotFoundException extends \Exception
+{
+
+}
