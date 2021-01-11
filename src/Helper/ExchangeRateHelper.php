a9e6146ced63baeeab39aff518106e90ec7a298b
MirayS <my@mirays.tech>
2021-01-11 23:27:52 +0300

    Added cache in ExchangeRateHelper

--- src/Helper/ExchangeRateHelper.php
+++ src/Helper/ExchangeRateHelper.php
@@ -6,10 +6,32 @@ namespace App\Helper;
 
 class ExchangeRateHelper
 {
+    private array $cache = [];
+
     public const USD_CURRENCY = "USD";
     public const GBP_CURRENCY = "GBP";
 
-    public function getRates(string $from, string $to): ?float {
+    public function getRates(string $from, string $to): ?float
+    {
+        return $this->getRatesFromCache($from, $to);
+    }
+
+    private function getRatesFromCache(string $from, string $to): ?float
+    {
+        if (isset($this->cache[$from]) && isset($this->cache[$from][$to]))
+        {
+            return $this->cache[$from][$to];
+        }
+        $result = $this->getRatesFromApi($from, $to);
+        if ($result != null)
+        {
+            $this->cache[$from][$to] = $result;
+        }
+        return $result;
+    }
+
+    private function getRatesFromApi(string $from, string $to) : ?float
+    {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "https://api.exchangeratesapi.io/latest?symbols=${to}&base=${from}");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
